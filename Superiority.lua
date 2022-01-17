2cc00e05c2690a5d595aa848c8ad420aac6134b8
NayyGod <97779130+NayyGod@users.noreply.github.com>
2022-01-17 09:31:11 -0500

    Update Superiority.lua

--- Superiority.lua
+++ Superiority.lua
@@ -1,7 +1,7 @@
 while true do
 local Animate = game.Players.LocalPlayer.Character.Animate
-Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=616158929"
-Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=616160636"
+Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=4417977954"
+Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=4417978624"
 Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=616168032"
 Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=616163682"
 game.Players.LocalPlayer.Character.Humanoid.Jump = false
