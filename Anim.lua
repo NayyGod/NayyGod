158721dc989d7b2ad99c1c56bfc8c98c35f7399f
NayyGod <97779130+NayyGod@users.noreply.github.com>
2022-01-22 19:57:06 -0500

    Update Anim.lua

--- Anim.lua
+++ Anim.lua
@@ -1,26 +1,4 @@
-local player=game.Players.LocalPlayer
-local mouse = player:GetMouse()
-
-local bodythurst=Instance.new("BodyVelocity", player.Character.Head)
-
-local speedy=false
-mouse.KeyDown:connect(function(key)
-if key:byte() == 0 then
-bodythurst.force=Vector3.new(0,0)
-end end)
-
-local player=game.Players.LocalPlayer
-local mouse = player:GetMouse()
-
-local bodythurst=Instance.new("BodyAngularVelocity", player.Character.Head)
-
-local speedy=false
-mouse.KeyDown:connect(function(key)
-if key:byte() == 0 then
-bodythurst.force=Vector3.new(0,0)
-end end)
-
-  local Anim = Instance.new("Animation")
+ local Anim = Instance.new("Animation")
 
       Anim.AnimationId = "rbxassetid://3338010159"
 
